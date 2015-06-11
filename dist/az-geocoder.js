d025db93a6c48a3b8d44e9e6120fc955494c6777
CloudNiner <andrew.fink.11@gmail.com>
2015-06-11 13:58:49 -0400

    Add basic development framework
    Gulp to test/lint/minify/uglify library
    Replace default Mocha test framework w/ Jasmine
    Tweak some defaults (editor, jshint, etc)

---
+++ dist/az-geocoder.js
@@ -0,0 +1,10 @@
+(function (angular) {
+
+  // Create all modules and define dependencies to make sure they exist
+  // and are loaded in the correct order to satisfy dependency injection
+  // before all nested files are concatenated by Gulp
+
+  // Modules
+  angular.module('azGeocoder', []);
+
+})(angular);
---
+++ test/unit/az-geocoder/az-geocoder.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('sample test for az-geocoder', function() {
+
+    angular.module('azGeocoder');
+
+    beforeEach(function() {
+
+    });
+
+    it('should ensure true is true', function() {
+        expect(true).toBe(true);
+    });
+});
